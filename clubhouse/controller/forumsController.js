582bbeb07110ba92f00faa7b95ed9b1e05cc950f
Shriharsh <shriharsh.dev@gmail.com>
2023-11-17 10:41:44 +0530

    Updated post and forums controller files

--- clubhouse/controller/forumsController.js
+++ clubhouse/controller/forumsController.js
@@ -1,12 +1,46 @@
 const asyncHandler = require('express-async-handler')
+const Forum = require('../models/forums')
+const Post = require('../models/posts')
 
 exports.forums_list = asyncHandler(async(req,res,next)=>{
-    res.send("FORUMS LIST")
+    try{
+        const forums = await Forum.find().exec()
+        res.render("forum_list",{
+            forums:forums
+        })
+    }catch(err){
+        return next(err)
+    }
+})
+
+exports.forum_posts = asyncHandler(async(req,res,next)=>{
+    try{
+        const forum = await Forum.findById(req.params.id).exec()
+        if(!forum){
+            return res.status(404).send('Forum not found');
+        }
+        const forumPosts = await Post.find({forum:forum}).exec()
+        res.render("forum_posts.ejs",{
+            forum:forum,
+            posts:forumPosts
+        })
+    }catch(err){
+        return next(err)
+    }
 })
 
 exports.forum_create_get = asyncHandler(async(req,res,next)=>{
-    res.send("Create forum... GET")
+    res.render("forum_create.ejs")
 })
 exports.forum_create_post = asyncHandler(async(req,res,next)=>{
-    res.send("Create forum... POST")
+    try{
+        const forum = new Forum({
+            title:req.body.title,
+            description:req.body.description
+        })
+        const result = await forum.save()
+        res.redirect("/clubhouse/forums")
+    } catch(err){
+        return next(err)
+    }
 })
--- clubhouse/controller/postController.js
+++ clubhouse/controller/postController.js
@@ -1,11 +1,48 @@
 const asyncHandler = require('express-async-handler')
+const Post = require('../models/posts')
+const Forum = require('../models/forums')
 
 exports.post_list = asyncHandler(async(req,res,next)=>{
-    res.send("POSTS LIST")
+    try{
+        const posts = await Post.find().exec()
+        res.render("post_list.ejs",{
+            posts:posts,
+        })
+    } catch(err){
+        return next(err)
+    }
+})
+exports.post_detail = asyncHandler(async(req,res,next)=>{
+    try{
+        const post = await Post.findById(req.params.id).exec()
+        res.render("post_detail.ejs",{
+            post:post
+        })
+    } catch(err){
+        return next(err)
+    }
 })
 exports.create_post_get = asyncHandler(async(req,res,next)=>{
-    res.send("Create post... GET")
+    try{
+        const forums = await Forum.find().exec()
+        res.render("post_create.ejs",{
+            forums:forums
+        })
+    } catch (err) {
+        return next(err)
+    }
 })
 exports.create_post_post = asyncHandler(async(req,res,next)=>{
-    res.send("Create post...POST")
+    try{
+        const post = new Post({
+            title:req.body.title,
+            post:req.body.post,
+            forum: Array.isArray(req.body.forum) ? req.body.forum : [req.body.forum]
+        })
+        console.log(post)
+        const result = await post.save()
+        res.redirect("/clubhouse/posts")
+    } catch(err){
+        return next(err)
+    }
 })
